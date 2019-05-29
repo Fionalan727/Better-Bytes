63395326f9f4d10a13ce61240a5470f127b5c759
vagrant <mikefyke@hotmail.com>
2019-05-29 13:22:51 +0000

    Added new seed files and buuil/image

---
+++ db/seeds/seed_ingredient_grocery_list.js
@@ -0,0 +1,34 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('ingredient_grocery_list').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('ingredient_grocery_list').insert([
+        {id: 1, grocery_list_id: 1, ingredient_id: 22},
+        {id: 2, grocery_list_id: 1, ingredient_id: 2},
+        {id: 3, grocery_list_id: 1, ingredient_id: 23},
+        {id: 4, grocery_list_id: 1, ingredient_id: 8},
+        {id: 5, grocery_list_id: 2, ingredient_id: 22},
+        {id: 6, grocery_list_id: 2, ingredient_id: 2},
+        {id: 7, grocery_list_id: 2, ingredient_id: 23},
+        {id: 8, grocery_list_id: 2, ingredient_id: 8},
+        {id: 9, grocery_list_id: 3, ingredient_id: 22},
+        {id: 10, grocery_list_id: 3, ingredient_id: 2},
+        {id: 11, grocery_list_id: 3, ingredient_id: 23},
+        {id: 12, grocery_list_id: 3, ingredient_id: 8},
+        {id: 13, grocery_list_id: 4, ingredient_id: 18},
+        {id: 14, grocery_list_id: 4, ingredient_id: 2},
+        {id: 15, grocery_list_id: 4, ingredient_id: 8},
+        {id: 16, grocery_list_id: 4, ingredient_id: 19},
+        {id: 17, grocery_list_id: 5, ingredient_id: 18},
+        {id: 18, grocery_list_id: 5, ingredient_id: 2},
+        {id: 19, grocery_list_id: 5, ingredient_id: 8},
+        {id: 20, grocery_list_id: 5, ingredient_id: 19},
+        {id: 21, grocery_list_id: 6, ingredient_id: 18},
+        {id: 22, grocery_list_id: 6, ingredient_id: 2},
+        {id: 23, grocery_list_id: 6, ingredient_id: 8},
+        {id: 24, grocery_list_id: 6, ingredient_id: 19}
+      ]);
+    });
+};
