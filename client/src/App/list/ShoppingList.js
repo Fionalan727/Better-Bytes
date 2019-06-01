6e8a105064c14ceef47dce0fdb69197e429292ee
minlucyzhao <minlucyzhao@gmail.com>
2019-06-01 11:59:07 +0000

    Create nested routing for shoppinglist page

--- client/src/App/list/ShoppingList.js
+++ client/src/App/list/ShoppingList.js
@@ -1,27 +1,88 @@
+
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import OneShop from "./OneShop";
 import BestDeal from "./BestDeal";
 
-class ShoppingList extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-        active: 'ONESHOP',
-        recipes: [],
-        ingredients: [],
-        numOfPeople: 2,
-        store: '',
-        total: 1234,
-    };
+// class ShoppingList extends Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//         active: 'ONESHOP',
+//         recipes: [],
+//         ingredients: [],
+//         numOfPeople: 2,
+//         store: '',
+//         total: 1234,
+//     };
+//   }
+
+  function ShoppingList() {
+    return (
+      <Router>
+        <div>
+          
+          {/* Linking the button to the path */}
+          <Link to = "/shoppinglist/onestop"> One Store </Link>
+          <Link to = "/shoppinglist/bestdeal"> Best Deal </Link>
+          
+          {/* Linking path to component */}
+          <Route path = "/shoppinglist/onestop" component = {Topics} />
+          <Route path = "/shoppinglist/bestdeal" component = {About} />
+        </div>
+      </Router>
+    );
   }
 
-  handleClick() {
-    var active = this.state.active;
-        var newActive = active === 'ONESHOP' ? 'BESTDEAL' : 'ONESHOP';
-        this.setState({
-            active: newActive
-        });
+  function Topics({ match }) {
+    return (
+      <div>
+        <h2>Topics</h2>
+        <ul>
+          <li>
+            <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/components`}>Components</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+          </li>
+        </ul>
+  
+        <Route path={`${match.path}/:topicId`} component={Topic} />
+        <Route
+          exact
+          path={match.path}
+          render={() => <h3>Please select a topic.</h3>}
+        />
+      </div>
+    );
   }
+  
+  function Topic({ match }) {
+    return (
+      <div>
+        <h3>{match.params.topicId}</h3>
+      </div>
+    );
+  }
+  
+  function About() {
+    return (
+      <div>
+        <h2>About</h2>
+      </div>
+    );
+  }
+  
+  // handleClick() {
+  //   var active = this.state.active;
+  //       var newActive = active === 'ONESHOP' ? 'BESTDEAL' : 'ONESHOP';
+  //       this.setState({
+  //           active: newActive
+  //       });
+  // }
 
   //changeStore() {}
   //this.setState({store: Walmart})
@@ -47,27 +108,27 @@ class ShoppingList extends Component {
   //
 
 
-  render() {    
-    var active = this.state.active;
+//   render() {    
+//     var active = this.state.active;
 
-    return (
-      <div className="App">
-        <div id="header">
-            <h1>Shopping List</h1>
-            <div>
-                {active === 'ONESHOP' ? (
-                    <OneShop />
-                ) : active === 'BESTDEAL' ? (
-                    <BestDeal />
-                ) : null}
-                <button type="button" onClick={this.handleClick}>
-                    Toggle
-                </button>
-            </div>
-          </div>
-      </div>
-    )
-  }
-}
+//     return (
+//       <div className="App">
+//         <div id="header">
+//             <h1>Shopping List</h1>
+//             <div>
+//                 {active === 'ONESHOP' ? (
+//                     <OneShop />
+//                 ) : active === 'BESTDEAL' ? (
+//                     <BestDeal />
+//                 ) : null}
+//                 <button type="button" onClick={this.handleClick}>
+//                     Toggle
+//                 </button>
+//             </div>
+//           </div>
+//       </div>
+//     )
+//   }
+// }
 
 export default ShoppingList;
