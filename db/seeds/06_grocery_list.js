3c65e669dceae9182468ff61994ae487c5a71aa3
minlucyzhao <minlucyzhao@gmail.com>
2019-06-02 14:17:36 +0000

    Add quantity_per_item column to store_ingredients seed file and made changes to other seed data

--- db/seeds/06_grocery_list.js
+++ db/seeds/06_grocery_list.js
@@ -5,12 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('grocery_list').insert([
-        {id: 1, cookie_id: 22222},
-        {id: 2, cookie_id: 22222},
-        {id: 3, cookie_id: 22222},
-        {id: 4, cookie_id: 22222},
-        {id: 5, cookie_id: 22222},
-        {id: 6, cookie_id: 22222}
+        {id: 1, cookie_id: 11111},
       ]);
     });
 };
--- db/seeds/seed_store_grocery_list.js
+++ db/seeds/seed_store_grocery_list.js
@@ -7,22 +7,7 @@ exports.seed = function(knex, Promise) {
       return knex('store_grocery_list').insert([
         {id: 1, store_id: 1, grocery_list_id: 1},
         {id: 2, store_id: 2, grocery_list_id: 1},
-        {id: 3, store_id: 3, grocery_list_id: 1},
-        {id: 4, store_id: 1, grocery_list_id: 2},
-        {id: 5, store_id: 2, grocery_list_id: 2},
-        {id: 6, store_id: 3, grocery_list_id: 2},
-        {id: 7, store_id: 1, grocery_list_id: 3},
-        {id: 8, store_id: 2, grocery_list_id: 3},
-        {id: 9, store_id: 3, grocery_list_id: 3},
-        {id: 10, store_id: 1, grocery_list_id: 4},
-        {id: 11, store_id: 2, grocery_list_id: 4},
-        {id: 12, store_id: 3, grocery_list_id: 4},
-        {id: 13, store_id: 1, grocery_list_id: 5},
-        {id: 14, store_id: 2, grocery_list_id: 5},
-        {id: 15, store_id: 3, grocery_list_id: 5},
-        {id: 16, store_id: 1, grocery_list_id: 6},
-        {id: 17, store_id: 2, grocery_list_id: 6},
-        {id: 18, store_id: 3, grocery_list_id: 6}
+        {id: 3, store_id: 3, grocery_list_id: 1}
       ]);
     });
 };
