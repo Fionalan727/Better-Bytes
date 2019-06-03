f1f2918c4a16007b9897f1f3ea63fc9340b60366
vagrant <mikefyke@hotmail.com>
2019-06-03 05:33:10 +0000

    Finsihed scraping 3 stores and Filtered cheapest option

--- routes/search.js
+++ routes/search.js
@@ -21,11 +21,12 @@ request(`https://www.metro.ca/en/search?filter=${query}&freeText=true`, (error, 
     if(!error && response.statusCode === 200){
       const $ = cheerio.load(html)
       let items = [], prices = [], quantities = [];
+      let testArray = [];
       
       $('.pt--name div').each((i, gl) => {
         const name = $(gl).text();
         console.log(name);
-        searchResults.name = name;
+        // searchResults.name = name;
         items.push(name);
       });
 
@@ -49,16 +50,106 @@ request(`https://www.metro.ca/en/search?filter=${query}&freeText=true`, (error, 
         quantities.push(unit_quantity);
       });
 
-console.log(items,prices,quantities)
-// write for loop here
-      // cart.push({searchResults: searchResults});
+      console.log(items,prices,quantities)
+
       var length = items.length;
       for (var i = 0; i<length; i++){
-      searchResults.push({id: "metro",name: items[i], price: prices[i], quantity: quantities[i]})
+        testArray.push({id: "Metro",name: items[i], price: prices[i], quantity: quantities[i]})
       }
+      const list = (testArray.sort((a, b) => (a.price > b.price) ? 1 : -1));
+      searchResults.push(list[0])
+      // res.json(searchResults);
+    }});
 
-      res.json(searchResults);
-}});
+    request(`https://www.metro.ca/en/search?filter=${query}&freeText=true`, (error, response, html) => {
+      if(!error && response.statusCode === 200){
+        const $ = cheerio.load(html)
+        let items = [], prices = [], quantities = [];
+        let testArray = [];
+        
+        $('.pt--name div').each((i, gl) => {
+          const name = $(gl).text();
+          console.log(name);
+          // searchResults.name = name;
+          items.push(name);
+        });
+  
+        $('.pi--main-price').each((i, el) => {
+          const price = $(el).text();
+          var regexprice = /\$\s?(\d+[\.\s,\dk]+)|(\d+[\.\s,\dk]+)\$/mig;
+          var foundprice = price.match(regexprice)[0].trim().slice(1);
+          console.log(foundprice)
+          prices.push(foundprice);
+        });
+  
+        $('.pi--main-price span.pi--price').each((i, fl) => {
+          const quantity = $(fl).text();
+          var unit_quantity = 0;
+          if(quantity.includes ("/")){
+            unit_quantity = quantity.substring(0, 1);
+          } else {
+            unit_quantity = 1;
+          }
+          console.log(unit_quantity)
+          quantities.push(unit_quantity);
+        });
+  
+        console.log(items,prices,quantities)
+  
+        var length = items.length;
+        for (var i = 0; i<length; i++){
+          testArray.push({id: "Walmart",name: items[i], price: prices[i], quantity: quantities[i]})
+        }
+        const list = (testArray.sort((a, b) => (a.price > b.price) ? 1 : -1));
+        searchResults.push(list[0])
+        // res.json(searchResults);
+      }});
+
+request(`https://www.nofrills.ca/search/1559526258197/page/~item/${query}/~sort/recommended/~selected/true`, (error, response, html) =>{
+  if(!error && response.statusCode === 200){
+    const $ = cheerio.load(html);
+    let items = [], prices = [], quantities = [];
+    let testArray = [];
+
+        $('.js-product-entry-name').each((i, gl) => {
+          const name = $(gl).text();
+          console.log(name);
+          // searchResults.name = name;
+          items.push(name);
+        });
+
+        $('.reg-price-text').each((i, el) => {
+          const price = $(el).text();
+          var regexprice = /\$\s?(\d+[\.\s,\dk]+)|(\d+[\.\s,\dk]+)\$/mig;
+          var foundprice = price.match(regexprice)[0].trim().slice(1);
+          prices.push(foundprice);
+        })
+
+            $('.reg-price-text').each((i, fl) => {
+              const quantity = $(fl).text();
+              var unit_quantity = 0;
+              if(quantity.includes ("/")){
+                unit_quantity = quantity.substring(0, 1);
+              } else {
+                unit_quantity = 1;
+              }
+              console.log(unit_quantity)
+              quantities.push(unit_quantity);
+            });
+
+            var length = 4;
+            for (var i = 0; i<length; i++){
+            testArray.push({id: "Loblaws",name: items[i], price: prices[i], quantity: quantities[i]})
+            }
+            const list2 = (testArray.sort((a, b) => (a.price > b.price) ? 1 : -1));
+            searchResults.push(list2[0])
+            console.log(searchResults)
+  };
+  const newseachResults = (searchResults.sort((a, b) => (a.price > b.price) ? 1 : -1));
+  console.log(newseachResults)
+  res.json(searchResults);
+});
+
 })
 return router;
 }
